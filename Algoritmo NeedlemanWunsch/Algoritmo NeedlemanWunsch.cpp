063d62ac6a7dd7dd4388fe4d6e04e43ad7af6369
Junior Usca <junior.usca@gmail.com>
2015-09-27 14:44:13 -0500

    alineaciones obtenidas, se obtiene una sola alineacion...la primera alineacion optima segun el orden diagonal, izquierda, arriba.

--- Algoritmo NeedlemanWunsch/Algoritmo NeedlemanWunsch.cpp
+++ Algoritmo NeedlemanWunsch/Algoritmo NeedlemanWunsch.cpp
@@ -13,7 +13,8 @@ int A = 0, G = 1, C = 2, T = 3;
 int esp = 5;
 
 // A    G   C   T
-int simularity[] = { 10, -1, -3, -4, //A
+int simularity[] = { 
+10, -1, -3, -4, //A
 -1,  7, -5, -3, //G
 -3, -5,  9,  0, //C
 -4, -3,  0,  8 };//T   
@@ -45,7 +46,7 @@ vector<int> convertirStringToVector(string str)
 void imprimir_matriz(int** M,int size1,int size2) {
 	for (int i = 0;i < size1;++i) {
 		for (int j = 0;j < size2;++j) {
-			cout << M[i][j] << ' ';
+			cout << M[i][j] << '\t';
 		}
 		cout << endl;
 	}
@@ -79,33 +80,23 @@ int main()
 	for (int i = 0; i <= size1; i++)
 		M[i] = new int[size2 + 1];
 
-
-	char ** traceback = new char *[size1 + 1];
-	for (int i = 0; i <= size1; i++)
-		traceback[i] = new char[size2 + 1];
-
 	M[0][0] = 0;
-	traceback[0][0] = 'n';
 
 	for (int j = 1; j <= size2; j++)
-	{
 		M[0][j] = -j * esp;
-		traceback[0][j] = '-';
-	}
 	for (int i = 1; i <= size1; i++)
-	{
 		M[i][0] = -i * esp;
-		traceback[i][0] = '|';
-	}
 
 	vector<int> sec_1 = convertirStringToVector(sec1);
 	vector<int> sec_2 = convertirStringToVector(sec2);
-
+	/// generamos la matriz
 	for (int i = 0; i < size1;++i) {
 		for (int j = 0; j < size2;++j) {
 			int k = M[i][j] + similitud(sec_1[i], sec_2[j]);
 			int l = M[i][j+1] - esp;
 			int m = M[i+1][j] - esp;
+
+			/// escogemos al mayor y lo guardamos en k
 			if (k >= l)
 				k = k;
 			else
@@ -115,6 +106,7 @@ int main()
 				k = k;
 			else
 				k = m;
+
 			M[i + 1][j + 1] = k;
 			
 		}
@@ -122,12 +114,51 @@ int main()
 
 	imprimir_matriz(M, size1 + 1, size2 + 1);
 
+	/// obtenemos las alineaciones
+
+	string alin_1 = ""; 
+	string alin_2 = "";
+	int i = size1; int j = size2;
+	while (i > 0 && j > 0) {
+		int score = M[i][j];
+		int diag = M[i-1][j-1];
+		int up = M[i][j-1];
+		int izq = M[i-1][j];
+		if (score == diag + similitud(sec_1[i - 1], sec_2[j - 1])) {
+			alin_1 = sec1[i - 1] + alin_1;
+			alin_2 = sec1[i - 1] + alin_2;
+			--i;--j;
+		}
+		else if (score == izq - esp) {
+			alin_1 = sec1[i - 1] + alin_1;
+			alin_2 = "-" + alin_2;
+			--i;
+		}
+		else if (score == up - esp) {
+			alin_1 = "-" + alin_1;
+			alin_2 = sec2[j - 1] + alin_2;
+			--j;
+		}
+	}
+	while (i > 0) {
+		alin_1 = sec1[i - 1] + alin_1;
+		alin_2 = "-" + alin_2;
+		--i;
+	}
+
+	while (j > 0) {
+		alin_1 = "-" + alin_1;
+		alin_2 = sec2[j - 1] + alin_2;
+		--j;
+	}
+
+	cout << "Alineacion 1: " << alin_1 << endl;
+	cout << "Alineacion 2: " << alin_2 << endl;
+
 	for (int i = 0; i <= size1; ++i) {
 		delete M[i];
-		delete traceback[i];
 	}
 	delete M;
-	delete traceback;
 
     return 0;
 }
